734f152775fc2b70c7c881f047430b3aeb313906
MustafoAlisherovich <mustafoalisherovic@gmail.com>
2026-03-20 04:01:57 +0500

    style: Shop page UI (full)

--- client/app/product/[id].tsx
+++ client/app/product/[id].tsx
@@ -32,13 +32,14 @@ export default function ProductDetails() {
 	const { toggleWishlist, isInWishlist } = useWishlist()
 
 	const fetchProduct = async () => {
-		setProduct(dummyProducts.find(product => product._id === id) as any)
+		const found = dummyProducts.find(product => product._id === id)
+		setProduct(found ?? null)
 		setLoading(false)
 	}
 
 	useEffect(() => {
 		fetchProduct()
-	}, [])
+	}, [id])
 
 	if (loading) {
 		return (
