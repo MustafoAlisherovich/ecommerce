f03430e69eadd666ab2ae9f6d8495d59f2b0ea0b
MustafoAlisherovich <mustafoalisherovic@gmail.com>
2026-03-23 03:16:50 +0500

    feat: clerk webhooks endpoint created

--- server/server.ts
+++ server/server.ts
@@ -2,11 +2,14 @@ import cors from 'cors'
 import 'dotenv/config'
 import express, { Request, Response } from 'express'
 import connectDB from './config/db.js'
+import { clerkWebhook } from './controllers/webhook.js'
 
 const app = express()
 
 // Connect to MongoDB
 await connectDB()
+
+app.post('/api/clerk', express.raw({ type: 'application/json' }), clerkWebhook)
 
 // Middleware
 app.use(cors())
